071dd2f03df2814ce27f9209e87c1c4d76884986
Jesse Hunt <jh.trumpet37@gmail.com>
2020-05-14 14:29:08 +0200

    - prepared section 4 assignment

--- Projects/section_4/src/App.js
+++ Projects/section_4/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+// App.js
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+      input_text: "",
+      text_length: 0,
+  };
+
+  changedTextHandler = (event) => {
+    this.setState({
+        input_text: event.target.value,
+        text_length: event.target.value.length,
+    });
+
+  };
+
+  render() {
+
+    return (
+        <div className="App">
+          <h1>Name Array App</h1>
+          <h2>Section 4 Assignment</h2>
+          <div>
+            <label htmlFor="input_text">Text Input:</label>
+            <br/>
+            <input type="text" name="input_text" onChange={this.changedTextHandler} value={this.state.input_text}/>
+          </div>
+          <p>The text is {this.state.text_length} characters long.</p>
+        </div>
+    )
+  }
 }
 
 export default App;
