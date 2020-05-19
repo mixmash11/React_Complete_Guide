acde8bce2c6de445c6274b94c3ef703b4c505e66
Jesse Hunt <jh.trumpet37@gmail.com>
2020-05-19 11:49:25 +0200

    - partial completion of project section 4

--- Projects/section_4/src/App.js
+++ Projects/section_4/src/App.js
@@ -1,36 +1,39 @@
 // App.js
 import React, {Component} from 'react';
+import ValidationComponent from "./ValidationComponent/ValidationComponent";
 import './App.css';
 
 class App extends Component {
-  state = {
-      input_text: "",
-      text_length: 0,
-  };
+    state = {
+        input_text: "",
+        text_length: 0,
+    };
 
-  changedTextHandler = (event) => {
-    this.setState({
-        input_text: event.target.value,
-        text_length: event.target.value.length,
-    });
+    changedTextHandler = (event) => {
+        this.setState({
+            input_text: event.target.value,
+            text_length: event.target.value.length,
+        });
 
-  };
+    };
 
-  render() {
+    render() {
 
-    return (
-        <div className="App">
-          <h1>Name Array App</h1>
-          <h2>Section 4 Assignment</h2>
-          <div>
-            <label htmlFor="input_text">Text Input:</label>
-            <br/>
-            <input type="text" name="input_text" onChange={this.changedTextHandler} value={this.state.input_text}/>
-          </div>
-          <p>The text is {this.state.text_length} characters long.</p>
-        </div>
-    )
-  }
+        return (
+            <div className="App">
+                <h1>Name Array App</h1>
+                <h2>Section 4 Assignment</h2>
+                <div>
+                    <label htmlFor="input_text">Text Input:</label>
+                    <br/>
+                    <input type="text" name="input_text" onChange={this.changedTextHandler}
+                           value={this.state.input_text}/>
+                </div>
+                <p>The text is {this.state.text_length} characters long.</p>
+                <ValidationComponent text_length={this.state.text_length}/>
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ Projects/section_4/src/ValidationComponent/ValidationComponent.js
@@ -0,0 +1,15 @@
+// ValidationComponent.js
+import React from "react";
+
+const ValidationComponent = (props) => {
+
+    let textComment = parseInt(props.text_length) >= 5 ? <p>Text long enough</p> : <p>Text too short</p>;
+
+    return (
+        <div>
+            {textComment}
+        </div>
+    )
+};
+
+export default ValidationComponent;
